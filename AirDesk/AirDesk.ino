5f65cc951bd88f0d1a06bc73e47a5aa777037393
gigrichj <gigrichj@yahoo.com>
2026-07-13 18:35:29 -0400

    Remove lv_demo_widgets call from AirDesk.ino
    Removed demo widgets function call from main code.

--- AirDesk/AirDesk.ino
+++ AirDesk/AirDesk.ino
@@ -5,7 +5,6 @@
 
 #include <lvgl.h>
 #include "lvgl_v8_port.h"
-#include <demos/lv_demos.h>
 
 using namespace esp_panel::drivers;
 using namespace esp_panel::board;
@@ -79,7 +78,7 @@ void setup()
      * Or try out a demo.
      * Don't forget to uncomment header and enable the demos in `lv_conf.h`. E.g. `LV_USE_DEMO_WIDGETS`
      */
-lv_demo_widgets();
+ 
     // lv_demo_benchmark();
     // lv_demo_music();
     // lv_demo_stress();
